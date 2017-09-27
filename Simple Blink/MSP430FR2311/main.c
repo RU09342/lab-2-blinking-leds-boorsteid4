3df3f42fce83f0d25889829617fa346bbb8f2f5c
Damon Boorstein <Damon Boorstein>
2017-09-26 21:00:39 -0400

    Updated some code & documentation in source files. Added to README.md

--- Simple Blink/MSP430FR2311/main.c
+++ Simple Blink/MSP430FR2311/main.c
@@ -5,12 +5,16 @@
 //|                                                                                                                                                 |
 //| Author: Damon Boorstein                                                                                                                         |
 //| Date Created: 9/20/17                                                                                                                           |
-//| Last Updated: 9/20/17                                                                                                                           |
+//| Last Updated: 9/26/17                                                                                                                           |
 //|_________________________________________________________________________________________________________________________________________________|
 
 #include <msp430fr2311.h>
 
-#define LED1     BIT0 // LED1 is defined as a 0x0001h
+#define LED1        BIT0 // LED1 is defined as a 0x0001h
+#define LED_SEL0    P1SEL0 // LED_SEL0 is defined as P1SEL0
+#define LED_SEL1    P1SEL1 // LED_SEL1 is defined as P1SEL1
+#define LED_DIR     P1DIR // LED_DIR is defined as P1DIR
+#define LED_OUT     P1OUT // LED_OUT is defined as P1OUT
 
 unsigned int count = 0;
 
@@ -20,13 +24,19 @@ void main(void)
     PM5CTL0 &= ~LOCKLPM5; // Disable the GPIO power-on default high-impedance mode to activate
                           // previously configured port settings
 
-    P1DIR |= LED1; // Set P1.0 as an output
-    P1OUT &= ~LED1; // LED1 is initialized as off
+    LED_SEL0 &= ~LED1; // Select I/O function for P1.0 by masking with LED1 and clearing LED_SEL0
+    LED_SEL1 &= ~LED1; //   ...and LED_SEL1
+    LED_DIR |= LED1; // Change P1.0 to an output by masking with LED1 and setting LED_DIR
+    LED_OUT &= ~LED1; // Clear P1.0 (turn LED1 off) by masking with LED1 and clearing LED_OUT
 
-    while(1) // Infinite loop
+    while(1) // This initiates an "infinite" loop for polling
     {
-        if(count % 1000 == 0) // Cycle every 1000 clock cycles
-            P1OUT ^= LED1; // Toggle LED1
+        if(count % 1000 == 0) // If count reaches a multiple of 1000...
+        {
+            LED_OUT ^= LED1; // Toggle P1.0 by masking with LED1 and xor'ing with LED_OUT
+            count = 0; // Reset the counter
+        }
+
         count++; // Increment the counter
     }
 
--- Simple Blink/MSP430G2553/main.c
+++ Simple Blink/MSP430G2553/main.c
@@ -5,12 +5,15 @@
 //|                                                                                                                                                 |
 //| Author: Damon Boorstein                                                                                                                         |
 //| Date Created: 9/12/17                                                                                                                           |
-//| Last Updated: 9/20/17                                                                                                                           |
+//| Last Updated: 9/26/17                                                                                                                           |
 //|_________________________________________________________________________________________________________________________________________________|
 
 #include <msp430g2553.h>
 
-#define LED1     BIT0 // LED1 is defined as a 0x0001h
+#define LED1    BIT0 // LED1 is defined as a 0x0001h
+#define LED_SEL P1SEL // LED_SEL is defined as P1SEL
+#define LED_DIR P1DIR // LED_DIR is defined as P1DIR
+#define LED_OUT P1OUT // LED_OUT is defined as P1OUT
 
 unsigned int count = 0;
 
@@ -18,13 +21,18 @@ void main(void)
 {
     WDTCTL = WDTPW + WDTHOLD; // Stop watchdog timer
 
-    P1DIR |= LED1; // Set P1.0 as an output
-    P1OUT &= ~LED1; // LED1 is initialized as off
+    LED_SEL &= ~LED1; // Select I/O function for P1.0 by masking with LED1 and clearing LED_SEL
+    LED_DIR |= LED1; // Change P1.0 to an output by masking with LED1 and setting LED_DIR
+    LED_OUT &= ~LED1; // Clear P1.0 (turn LED1 off) by masking with LED1 and clearing LED_OUT
 
-    while(1) // Infinite loop
+    while(1) // This initiates an "infinite" loop for polling
     {
-        if(count % 1000 == 0) // Cycle every 1000 clock cycles
-            P1OUT ^= LED1; // Toggle LED1
+        if(count % 1000 == 0) // If count reaches a multiple of 1000...
+        {
+            LED_OUT ^= LED1; // Toggle P1.0 by masking with LED1 and xor'ing with LED_OUT
+            count = 0; // Reset the counter
+        }
+
         count++; // Increment the counter
     }
 
